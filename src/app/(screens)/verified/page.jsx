f6e5355c268aa035304d4b747e65dd5f9c72a219
thailychan <c.thaily@dgc.gov.kh>
2024-08-29 17:07:19 +0700

    chore: add iframe on verified page.

--- src/app/(screens)/verified/page.jsx
+++ src/app/(screens)/verified/page.jsx
@@ -3,48 +3,7 @@ import Image from 'next/image';
 const VerifiedScreen = () => {
     return (
         <>
-            <Box
-            >
-                <Typography
-                    variant="h6"
-                    sx={{
-                        fontSize: '32px', fontWeight: 'bold', color: "#2E7D32", display: 'flex',
-                        justifyContent: 'center', marginTop: "32px"
-                    }}
-                >
-                    ឃ្យូ.អ.កូដ ស្ដង់ដា ត្រឹមត្រូវ
-                </Typography>
-                <Typography
-                    variant="h6"
-                    sx={{
-                        fontSize: '16px', fontWeight: 'semibold', display: 'flex',
-                        justifyContent: 'center', marginTop: "12px"
-                    }}
-                >
-                    ចេញដោយ សាកលវិទ្យាល័យការពារជាតិ
-                </Typography>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        marginTop: '20px',
-                        width: '100%',
-                    }}
-                >
-                    <Image
-                        src="/certificate.jfif"
-                        alt="Verify Logo"
-                        width={1400}
-                        height={100}
-                        priority
-                        style={{
-                            maxWidth: '100%',
-                            height: 'auto',
-                        }}
-                    />
-                </Box>
-
-            </Box>
+            <iframe src="https://dev.verifykh.com/verify/?isVerifyApp=true" frameborder="0"></iframe>
         </>
     );
 };
